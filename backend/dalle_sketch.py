72119ad9a43e8bb7ca43897468894655c0e2a827
Sebastian Gilits <sep.gil@gmail.com>
2022-05-01 12:15:03 +0000

    Fix jax static params issues

--- backend/dalle_sketch.py
+++ backend/dalle_sketch.py
@@ -23,119 +23,128 @@ VQGAN_COMMIT_ID = "e93a26e7707683d349bf5d5c41c5b0ef69b677a9"
 CLIP_REPO = "openai/clip-vit-large-patch14"
 CLIP_COMMIT_ID = None
 
-class DalleSketcher:
+model = DalleBart.from_pretrained(DALLE_MODEL, revision=DALLE_COMMIT_ID)
+vqgan = VQModel.from_pretrained(VQGAN_REPO, revision=VQGAN_COMMIT_ID)
+clip = FlaxCLIPModel.from_pretrained(CLIP_REPO, revision=CLIP_COMMIT_ID)
+clip_processor = CLIPProcessor.from_pretrained(CLIP_REPO, revision=CLIP_COMMIT_ID)
 
-    def __init__(self) -> None:
+model._params = replicate(model.params)
+vqgan._params = replicate(vqgan.params)
+clip._params = replicate(clip.params)
 
-        self.__model = DalleBart.from_pretrained(DALLE_MODEL, revision=DALLE_COMMIT_ID)
-        self.__vqgan = VQModel.from_pretrained(VQGAN_REPO, revision=VQGAN_COMMIT_ID)
-        self.__clip = FlaxCLIPModel.from_pretrained(CLIP_REPO, revision=CLIP_COMMIT_ID)
-        self.__clip_processor = CLIPProcessor.from_pretrained(CLIP_REPO, revision=CLIP_COMMIT_ID)
-        
-        self.__model._params = replicate(self.__model.params)
-        self.__vqgan._params = replicate(self.__vqgan.params)
-        self.__clip._params = replicate(self.__clip.params)
+words = np.load("./assets/words.npy", allow_pickle=True)
+adjectives = np.load("./assets/adjectives.npy", allow_pickle=True)
+colors = np.load("./assets/colors.npy", allow_pickle=True)
 
-        self.__words = np.load("./words.npy", allow_pickle=True)
-        self.__adjectives = np.load("./adjectives.npy", allow_pickle=True)
-        self.__colors = np.load("./colors.npy", allow_pickle=True)
+@partial(jax.pmap, axis_name="batch", static_broadcasted_argnums=(3, 4, 5, 6))
+def p_generate(
+    tokenized_prompt, key, params, top_k, top_p, temperature, condition_scale
+):
+    """
+    Model inference.
+    """
+    return model.generate(
+        **tokenized_prompt,
+        prng_key=key,
+        params=params,
+        top_k=top_k,
+        top_p=top_p,
+        temperature=temperature,
+        condition_scale=condition_scale,
+    )
 
-    @partial(jax.pmap, axis_name="batch", static_broadcasted_argnums=(3, 4, 5, 6))
-    def p_generate(
-        self, tokenized_prompt, key, params, top_k, top_p, temperature, condition_scale
-    ):
-        """
-        Model inference.
-        """
-        return self.__model.generate(
-            **tokenized_prompt,
-            prng_key=key,
-            params=params,
-            top_k=top_k,
-            top_p=top_p,
-            temperature=temperature,
-            condition_scale=condition_scale,
-        )
+# decode image
 
-    # decode image
+@partial(jax.pmap, axis_name="batch")
+def p_decode(indices, params):
+    return vqgan.decode_code(indices, params=params)
 
-    @partial(jax.pmap, axis_name="batch")
-    def p_decode(self, indices, params):
-        return self.__vqgan.decode_code(indices, params=params)
+# score images
 
-    # score images
-
-    @partial(jax.pmap, axis_name="batch")
-    def p_clip(self, inputs, params):
-        logits = self.__clip(params=params, **inputs).logits_per_image
-        return logits
-
-    def generate_prompt(self, doodle=True):
-        item = np.random.choice(self.__words)
-        color = np.random.choice(self.__colors)
-        adj = np.random.choice(self.__adjectives)
-
-        prompt = f"{adj} {color} {item}"
-        if doodle:
-            prompt += " doodle"
-
-        return item, prompt
-
-    def draw_sketch(self, prompt, n_predictions=5):
-        # create a random key
-        seed = random.randint(0, 2**32 - 1)
-        key = jax.random.PRNGKey(seed)
-
-        # Load the text processor and create a tokenized text.
-        processor = DalleBartProcessor.from_pretrained(DALLE_MODEL, revision=DALLE_COMMIT_ID)
-        tokenized_prompt = processor([prompt])
-        tokenized_prompt = replicate(tokenized_prompt)
+@partial(jax.pmap, axis_name="batch")
+def p_clip(inputs, params):
+    logits = clip(params=params, **inputs).logits_per_image
+    return logits
 
 
-        # We can customize top_k/top_p used for generating samples
-        gen_top_k = None
-        gen_top_p = None
-        temperature = 0.85
-        cond_scale = 3.0
+def generate_prompt(doodle=True):
+    item = np.random.choice(words)
+    color = np.random.choice(colors)
+    adj = np.random.choice(adjectives)
 
+    prompt = f"{adj} {color} {item}"
+    if doodle:
+        prompt += " doodle"
+
+    return item, prompt
+
+def draw_sketch(prompt, n_predictions=5):
+    # create a random key
+    seed = random.randint(0, 2**32 - 1)
+    key = jax.random.PRNGKey(seed)
+
+    # Load the text processor and create a tokenized text.
+    processor = DalleBartProcessor.from_pretrained(DALLE_MODEL, revision=DALLE_COMMIT_ID)
+    tokenized_prompt = processor([prompt])
+    tokenized_prompt = replicate(tokenized_prompt)
+
+
+    # We can customize top_k/top_p used for generating samples
+    gen_top_k = None
+    gen_top_p = None
+    temperature = 0.85
+    cond_scale = 3.0
+
+    # generate images
+    images = []
+    for i in range(max(n_predictions // jax.device_count(), 1)):
+        # get a new key
+        key, subkey = jax.random.split(key)
         # generate images
-        images = []
-        for i in range(max(n_predictions // jax.device_count(), 1)):
-            # get a new key
-            key, subkey = jax.random.split(key)
-            # generate images
-            encoded_images = self.p_generate(
-                tokenized_prompt,
-                shard_prng_key(subkey),
-                self.__model.params,
-                gen_top_k,
-                gen_top_p,
-                temperature,
-                cond_scale,
-            )
-            # remove BOS
-            encoded_images = encoded_images.sequences[..., 1:]
-            # decode images
-            decoded_images = self.p_decode(encoded_images, self.__vqgan.params)
-            decoded_images = decoded_images.clip(0.0, 1.0).reshape((-1, 256, 256, 3))
-            for img in decoded_images:
-                images.append(Image.fromarray(np.asarray(img * 255, dtype=np.uint8)))
+        encoded_images = p_generate(
+            tokenized_prompt,
+            shard_prng_key(subkey),
+            model.params,
+            gen_top_k,
+            gen_top_p,
+            temperature,
+            cond_scale,
+        )
+        # remove BOS
+        encoded_images = encoded_images.sequences[..., 1:]
+        # decode images
+        decoded_images = p_decode(encoded_images, vqgan.params)
+        decoded_images = decoded_images.clip(0.0, 1.0).reshape((-1, 256, 256, 3))
+        for img in decoded_images:
+            images.append(Image.fromarray(np.asarray(img * 255, dtype=np.uint8)))
 
-            from flax.training.common_utils import shard
+        from flax.training.common_utils import shard
 
-        # get clip scores
-        clip_inputs = self.__clip_processor(
-            text=[prompt] * jax.device_count(),
-            images=images,
-            return_tensors="np",
-            padding="max_length",
-            max_length=77,
-            truncation=True,
-        ).data
-        logits = self.p_clip(shard(clip_inputs), self.__clip.params)
-        logits = logits.squeeze().flatten()
+    # get clip scores
+    clip_inputs = clip_processor(
+        text=[prompt] * jax.device_count(),
+        images=images,
+        return_tensors="np",
+        padding="max_length",
+        max_length=77,
+        truncation=True,
+    ).data
+    logits = p_clip(shard(clip_inputs), clip.params)
+    logits = logits.squeeze().flatten()
 
-        indices = logits.argsort()
-        sorted_images = [images[idx] for idx in indices]
-        sorted_score = logits[indices]
-        return prompt, sorted_images, sorted_score
+    indices = logits.argsort()
+    sorted_images = [images[idx] for idx in indices]
+    sorted_score = logits[indices]
+    return prompt, sorted_images, sorted_score
+    
+
+
+if __name__ == "__main__":
+    item, prompt = generate_prompt()
+    n_prompt, images, score = draw_sketch(prompt, 5)
+
+    print(item)
+    print(n_prompt)
+    for i in range(len(images)):
+        print(score[i])
+        images[i].save(f"{item}{i}.png", format="png")
