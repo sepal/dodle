90b7f3d8951244b64e6c79e200cf6764f18b8016
Sebastian <sep.gil@gmail.com>
2022-05-02 16:59:12 +0200

    feat(game_manager): Implement game_server function.

---
+++ game_manager/game_server/game_server.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log"
+	"path/filepath"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/sepal/dodle/game_manager/dodle"
+)
+
+const BUCKET = "dodle"
+
+type GameResponse struct {
+	Word   string `json:"word"`
+	Images string `json:"images"`
+}
+
+var game dodle.GameData
+
+func HandleRequest(request events.APIGatewayProxyRequest) (g GameResponse, err error) {
+	g.Word = game.Word
+
+	for _, f := range game.Files {
+		fp := filepath.Base(f)
+		g.Images = fp
+	}
+
+	return g, err
+}
+
+func main() {
+	dodle.AWS_PREFIX = "testing/"
+
+	session, err := session.NewSession(&aws.Config{
+		Region: aws.String("eu-central-1"),
+	})
+
+	if err != nil {
+		log.Fatalf("Problem creating an AWS session: %s", err)
+	}
+
+	game, err := dodle.GetNextGame(session, BUCKET)
+
+	if err != nil {
+		log.Fatalf("Could not load next game due to: %s", err)
+	}
+
+	err = game.LoadImages(session)
+
+	if err != nil {
+		log.Fatalf("Could not load game images due to: %s", err)
+	}
+
+	lambda.Start(HandleRequest)
+}
---
+++ game_manager/game_server/go.mod
@@ -0,0 +1,13 @@
+module github.com/sepal/game_manager/game_serve
+
+go 1.18
+
+require (
+	github.com/aws/aws-lambda-go v1.31.1
+	github.com/sepal/dodle/game_manager/dodle v0.0.0-20220502144740-c9c924ee02e4
+)
+
+require (
+	github.com/aws/aws-sdk-go v1.44.4 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
