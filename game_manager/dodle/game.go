c9c924ee02e48e4a14afd4057d850b7718448029
Sebastian <sep.gil@gmail.com>
2022-05-02 16:47:40 +0200

    feat(game_manager): Implement dodle lib

---
+++ game_manager/dodle/game.go
@@ -0,0 +1,160 @@
+package dodle
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+)
+
+var AWS_PREFIX = "game/"
+
+type GameData struct {
+	Word   string    `json:"word"`
+	Prompt string    `json:"prompt"`
+	Scores []float64 `json:"scores"`
+	Files  []string  `json:"files"`
+	bucket string
+	prefix string
+}
+
+var CurrentTime = func() time.Time {
+	return time.Now()
+}
+
+func LoadGame(session *session.Session, bucket string, name string) (g *GameData, e error) {
+	buff := &aws.WriteAtBuffer{}
+
+	key := AWS_PREFIX + name + "/game.json"
+	fmt.Println(key)
+
+	downloader := s3manager.NewDownloader(session)
+	numBytes, err := downloader.Download(buff, &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if numBytes < 1 {
+		return nil, errors.New("Download game data with zero bytes")
+	}
+
+	err = json.Unmarshal(buff.Bytes(), &g)
+
+	if err != nil {
+		return nil, err
+	}
+
+	g.bucket = bucket
+	g.prefix = AWS_PREFIX + name
+
+	return g, e
+}
+
+func (g *GameData) LoadImages(session *session.Session) error {
+	downloader := s3manager.NewDownloader(session)
+
+	tempDir, err := os.MkdirTemp("", "dodle")
+
+	if err != nil {
+		return err
+	}
+
+	for i, f := range g.Files {
+		fileName := fmt.Sprintf("image%s.png", strconv.Itoa(i))
+
+		filePath := filepath.Join(tempDir, fileName)
+		file, err := os.Create(filePath)
+
+		if err != nil {
+			return err
+		}
+
+		key := fmt.Sprintf("%s/%s", g.prefix, f)
+
+		numBytes, err := downloader.Download(file, &s3.GetObjectInput{
+			Bucket: aws.String(g.bucket),
+			Key:    aws.String(key),
+		})
+
+		if err != nil {
+			return err
+		}
+
+		g.Files[i] = filePath
+
+		log.Printf("Downloaded %d bytes image %s for game %s\n", int(numBytes), f, g.Word)
+	}
+
+	return nil
+}
+
+func ListGames(session *session.Session, bucket string) (games []string, err error) {
+	svc := s3.New(session)
+	resp, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{
+		Bucket: aws.String(bucket),
+		Prefix: aws.String(AWS_PREFIX),
+	})
+
+	if err != nil {
+		return games, err
+	}
+
+	for _, element := range resp.Contents {
+		parts := strings.Split(*element.Key, "/")
+		if len(parts) >= 3 && parts[2] == "game.json" {
+			games = append(games, parts[1])
+		}
+	}
+
+	return games, nil
+}
+
+func GetNextGame(session *session.Session, bucket string) (g *GameData, err error) {
+	games, err := ListGames(session, bucket)
+
+	if err != nil {
+		return nil, err
+	}
+
+	ct := CurrentTime().Unix()
+
+	var next_game int64
+
+	next_game = 0
+
+	for _, name := range games {
+		epoch, err := strconv.ParseInt(name, 10, 0)
+
+		if err != nil {
+			return nil, err
+		}
+
+		if next_game == 0 && !(epoch > ct) {
+			next_game = epoch
+		}
+	}
+
+	if next_game == 0 {
+		return nil, errors.New("No game found")
+	}
+
+	name := strconv.FormatInt(next_game, 10)
+
+	log.Printf("Loading game %s", name)
+
+	return LoadGame(session, bucket, name)
+}
---
+++ game_manager/dodle/game_test.go
@@ -0,0 +1,128 @@
+package dodle
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+)
+
+const BUCKET = "dodle"
+
+func GetSession() (*session.Session, error) {
+	AWS_PREFIX = "testing/"
+	return session.NewSession(&aws.Config{
+		Region: aws.String("eu-central-1"),
+	})
+}
+
+func TestLoadGame(t *testing.T) {
+	sess, err := GetSession()
+
+	if err != nil {
+		t.Fatalf(`Error while loading game: "%s"`, err)
+	}
+
+	game, err := LoadGame(sess, BUCKET, "1651363200")
+
+	if err != nil {
+		t.Fatalf(`Error while loading game: "%s"`, err)
+	}
+
+	if game == nil {
+		t.Fatal(`Game not loaded`)
+	}
+
+	if game.Word != "toad" {
+		t.Fatalf(`Wanted game with word toad got "%s"`, game.Word)
+	}
+}
+
+func TestLoadGameImages(t *testing.T) {
+	sess, err := GetSession()
+
+	if err != nil {
+		t.Fatalf(`Error while loading game: "%s"`, err)
+	}
+
+	game, err := LoadGame(sess, BUCKET, "1651363200")
+
+	if err != nil {
+		t.Fatalf(`Error while loading game: "%s"`, err)
+	}
+
+	err = game.LoadImages(sess)
+
+	if err != nil {
+		t.Fatalf(`Error while loading game: "%s"`, err)
+	}
+
+	if _, err = os.Stat(game.Files[0]); err != nil {
+		t.Fatalf(`Error while checking if the first game image was downloaded "%s"`, err)
+	}
+}
+
+func TestListGames(t *testing.T) {
+	session, err := GetSession()
+
+	if err != nil {
+		t.Fatalf(`Error while loading game: "%s"`, err)
+	}
+	games, err := ListGames(session, BUCKET)
+
+	if err != nil {
+		t.Fatalf(`Error while listing games: "%s"`, err)
+	}
+
+	if len(games) < 1 {
+		t.Fatalf(`Expected at least one game, got "%d" games`, len(games))
+	}
+}
+
+func TestGetNextGame(t *testing.T) {
+	session, err := GetSession()
+
+	if err != nil {
+		t.Fatalf(`Error while loading game: "%s"`, err)
+	}
+
+	CurrentTime = func() time.Time {
+		return time.Date(2022, 05, 01, 00, 00, 00, 0, time.UTC)
+	}
+
+	g, err := GetNextGame(session, BUCKET)
+
+	if err != nil {
+		t.Fatalf(`Error while listing games: "%s"`, err)
+	}
+
+	if g.Word != "toad" {
+		t.Fatalf(`Expected game with word "toad", got "%s"`, g.Word)
+	}
+
+	CurrentTime = func() time.Time {
+		return time.Date(2022, 05, 01, 23, 59, 59, 0, time.UTC)
+	}
+
+	g, err = GetNextGame(session, BUCKET)
+
+	if err != nil {
+		t.Fatalf(`Error while listing games: "%s"`, err)
+	}
+
+	if g.Word != "toad" {
+		t.Fatalf(`Expected game with word "toad", got "%s"`, g.Word)
+	}
+
+	CurrentTime = func() time.Time {
+		return time.Date(2022, 04, 29, 23, 59, 59, 0, time.UTC)
+	}
+
+	g, err = GetNextGame(session, BUCKET)
+
+	if err == nil {
+		t.Fatalf("Should have not found a game, found %s", g.Word)
+	}
+}
