613c9493958c58ef0ea5664fad2e80a41b1186d8
Sebastian <sep.gil@gmail.com>
2022-05-04 20:17:38 +0200

    feat(frontend): Improve the canvas styling.

--- frontend/components/Canvas.tsx
+++ frontend/components/Canvas.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import styled from "styled-components";
+import Easel from "./Easel.svg"
+
 
 type CanvasInput = {
   image: string;
@@ -7,21 +9,39 @@ type CanvasInput = {
 
 const Frame = styled.div`
     display: block;
-    width: 256px;
-    height: 256px;
     margin: 0 auto;
+    position: relative;
 `;
+
+const ImageWrapper = styled.div`
+  position: absolute;
+  left: 6px;
+  top: 115px;
+  width: 300px;
+  height: 300px;
+  margin: 0px;
+  padding: 40px;
+  background: rgb(255,255,255);
+  background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(240,240,240,1) 100%); 
+  border: 4px solid #eee;
+`
+
 
 export default function Canvas({ image }: CanvasInput) {
   return (
     <Frame>
-      <Image
-        alt="A.I. drawing"
-        src={image}
-        quality={100}
-        width={256}
-        height={256}
-      />
+      <div>
+        <Easel />
+      </div>
+      <ImageWrapper>
+        <Image
+          alt="A.I. drawing"
+          src={image}
+          quality={100}
+          width={256}
+          height={256}
+        />
+      </ImageWrapper>
     </Frame>
   );
 }
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   compiler: {
   // Enables the styled-components SWC transform
     styledComponents: true
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
   }
 }
 
