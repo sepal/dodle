9c9fdc0577256eeb8f137979c5d34dc80484831a
Sebastian <sep.gil@gmail.com>
2022-05-05 17:29:59 +0200

    fix(frontend): Fix wrong url for deployment.

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -54,9 +54,11 @@ const Home: NextPage<HomeProps> = ({game} : HomeProps) => {
 };
 
 export async function getServerSideProps() {
+  const url = process.env.VERCEL_URL ?? "http://localhost:3000";
+  console.log(process.env.VERCEL_URL);
   // Call an external API endpoint to get posts.
   // You can use any data fetching library
-  const res = await fetch("http://localhost:3000/api/game");
+  const res = await fetch(`${url}/api/game`);
   const game = await res.json();
 
   // By returning { props: { posts } }, the Blog component
