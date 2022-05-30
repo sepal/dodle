bac59b642ddb6be592b709ec8194a3b5d740a73f
Sebastian Gilits <sep.gil@gmail.com>
2022-05-30 20:16:08 +0200

    Introduce repo helper functions

--- game_manager/dodle/repository.go
+++ game_manager/dodle/repository.go
@@ -42,13 +42,27 @@ type Repository interface {
 	CreateRound(ctx context.Context, input RoundFactory) (*Round, error)
 	GetRound(ctx context.Context, id int64) (*Round, error)
 	GetRoundByTime(ctx context.Context, date *time.Time) (*Round, error)
-	getNextEmptyDate(ctx context.Context) int64
 	GetRoundImage(ctx context.Context, level int) ([]byte, error)
 }
 
 // CreateRoundRepository creates a new repository to handle rounds.
 func CreateRoundRepository(db *bun.DB, session *session.Session) *RoundRepository {
 	return &RoundRepository{db, session, nil}
+}
+
+// createImageScores is a private function which creates all image scores for a round.
+func (r *RoundRepository) createImageScores(ctx context.Context, scores []float64) (*ImageScore, error) {
+	return nil, nil
+}
+
+// getNextEmptyDate will return the next empty date for inserting a game.
+func (r RoundRepository) getNextEmptyDate(ctx context.Context) int64 {
+	return 0
+}
+
+// createImageScores is a private function which creates all image entries for a round.
+func (r *RoundRepository) createImageEntries(ctx context.Context, bucket string, keys []string) (*ImageEntries, error) {
+	return nil, nil
 }
 
 // CreateRound creates a new round based on the given round input data. The
@@ -65,11 +79,6 @@ func (r RoundRepository) GetRound(ctx context.Context, id int64) (*Round, error)
 // GetGameByTime gets a certain round given the certain time.
 func (r RoundRepository) GetGameByTime(ctx context.Context, date *time.Time) (*Round, error) {
 	return nil, nil
-}
-
-// getNextEmptyDate will return the next empty date for inserting a game.
-func (r RoundRepository) getNextEmptyDate(ctx context.Context) int64 {
-	return 0
 }
 
 // GetRoundImage returns a byte slice containing the image data for the given level and game.
