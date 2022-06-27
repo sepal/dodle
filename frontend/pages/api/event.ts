ca88d84c3535926b9323bf5c8a304679ecbfa538
Sebastian Gilits <sep.gil@gmail.com>
2022-06-27 13:28:16 +0200

    Fix parsing event auth config.

--- frontend/pages/api/event.ts
+++ frontend/pages/api/event.ts
@@ -10,33 +10,33 @@ export default async function handler(
     const reqData: GameEvent = JSON.parse(req.body);
 
     const broker = process.env['EVENT_BROKER'] ?? "localhost:9092";
-    const ssl : boolean = process.env['EVENT_SSL'] == "1" ? true : false;
+    const ssl: boolean = process.env['EVENT_SSL'] == "1" ? true : false;
     const authorization = process.env["EVENT_AUTHORIZATION"];
 
     let config: any = {
       brokers: [broker],
     };
 
-    if (ssl == true) {
-      config['ssl'] = true; 
-    }
-
     if (authorization) {
       const auth = JSON.parse(authorization);
-      config = {...config, auth}
+      config = { ...config, ...auth };
+    }
+
+    if (ssl == true) {
+      config['ssl'] = true;
     }
 
     const kafka = new Kafka(config);
 
     const producer = kafka.producer();
     await producer.connect();
+
     await producer.send({
       topic: 'dodle_user_event',
       messages: [
         { value: JSON.stringify(reqData) },
       ],
     });
-
     await producer.disconnect();
   }
 }
