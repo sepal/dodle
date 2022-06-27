3ae2d7757b5023e1db673a8076d184abd7d8f216
Sebastian Gilits <sep.gil@gmail.com>
2022-06-27 12:49:29 +0200

    Rename broker config vars

--- frontend/pages/api/event.ts
+++ frontend/pages/api/event.ts
@@ -9,9 +9,9 @@ export default async function handler(
   if (req.method === 'POST') {
     const reqData: GameEvent = JSON.parse(req.body);
 
-    const broker = process.env['TRACK_BROKER'] ?? "localhost:9092";
-    const ssl : boolean = process.env['TRACK_SSL'] == "1" ? true : false;
-    const authorization = process.env["TRACK_AUTHORIZATION"];
+    const broker = process.env['EVENT_BROKER'] ?? "localhost:9092";
+    const ssl : boolean = process.env['EVENT_SSL'] == "1" ? true : false;
+    const authorization = process.env["EVENT_AUTHORIZATION"];
 
     let config: any = {
       brokers: [broker],
@@ -31,7 +31,7 @@ export default async function handler(
     const producer = kafka.producer();
     await producer.connect();
     await producer.send({
-      topic: 'dodle_tracking',
+      topic: 'dodle_user_event',
       messages: [
         { value: JSON.stringify(reqData) },
       ],
