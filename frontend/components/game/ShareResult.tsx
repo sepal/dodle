abb6e2671a773467555f6f4395c87c63114bc061
Sebastian Gilits <sep.gil@gmail.com>
2022-07-13 14:49:51 +0200

    fix(frotend): Add copied message to share results button.

--- frontend/components/game/ShareResult.tsx
+++ frontend/components/game/ShareResult.tsx
@@ -1,5 +1,8 @@
 import Button from "components/atoms/Button";
 import { getShareText } from "lib/share_results";
+import { useState } from "react";
+import ShareIcon from './icon--share.svg';
+
 
 interface Props {
     solution: string
@@ -9,15 +12,25 @@ interface Props {
 }
 
 export default function ShareResult({solution, guesses, maxGuesses, gameId} : Props) {
+    const [copied, setCopied] = useState<boolean>(false);
+
     const handleOnClick = () => {
         const result = getShareText(solution, guesses, maxGuesses, gameId);
         navigator.clipboard.writeText(result)
+        setCopied(true);
+
+        window.setTimeout(() => {
+            setCopied(false);
+        }, 1500);
     }
 
+    const label = copied ? "Results copied to clip board!" : (
+        <>Share results <ShareIcon style={{verticalAlign: "middle"}} /></>
+    );
 
     return (
         <>
-            <Button onClick={handleOnClick}>Share result!</Button>
+            <Button onClick={handleOnClick}>{label}</Button>
         </>
     )
 }
